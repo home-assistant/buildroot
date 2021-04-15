28a2450108743762b3b43629c86248f5fe45aa70
Stefan Agner <stefan@agner.ch>
2021-04-15 17:22:59 +0200

    package/docker-proxy: bump version to b3507428be5b
    Which is the version used by docker 20.10.6:
    https://github.com/moby/moby/commit/88470052e7d42f3dc774442241fd6bab817876f6
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>

--- package/docker-proxy/docker-proxy.mk
+++ package/docker-proxy/docker-proxy.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-DOCKER_PROXY_VERSION = 55685ba49593e67f5e1c8180539379b16736c25e
-DOCKER_PROXY_SITE = $(call github,docker,libnetwork,$(DOCKER_PROXY_VERSION))
+DOCKER_PROXY_VERSION = b3507428be5b458cb0e2b4086b13531fb0706e46
+DOCKER_PROXY_SITE = $(call github,moby,libnetwork,$(DOCKER_PROXY_VERSION))
 
 DOCKER_PROXY_LICENSE = Apache-2.0
 DOCKER_PROXY_LICENSE_FILES = LICENSE

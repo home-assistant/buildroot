f10577b83679910e52f9b90b9d943eceb4a4f0a1
Stefan Agner <stefan@agner.ch>
2021-09-07 21:32:00 +0200

    package/rpi-firmware: bump version to 1.20210805
    Update rpi-firmware to latest version (tag 1.20210805).
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 33e09977ff3b02967d191801e3361224455acb9d
+RPI_FIRMWARE_VERSION = 8939bde9fc31e88847061cb348d74af44767b136
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom

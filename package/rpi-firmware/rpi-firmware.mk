3ee68f22c1402e7c0564ebdfeb4a74cbe9fafc2c
Stefan Agner <stefan@agner.ch>
2022-01-26 12:44:18 +0100

    package/rpi-firmware: bump version to 1.20220120
    Bump to latest Raspberry Pi firmware release 1.20220120.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 3f20b832b27cd730deb6419b570f31a98167eef6
+RPI_FIRMWARE_VERSION = c6d56567ff6ef17fd85159770f22abcf2c5953ed
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom

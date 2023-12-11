150b05d3af783ca19f006b6369e3ca36ad70a0b2
Stefan Agner <stefan@agner.ch>
2023-12-11 22:30:01 +0100

    linux: fix add in-tree device tree overlay support
    Make sure the target directory gets created. Also deploy the overlays
    in all kernel deployment cases as well.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>

--- linux/linux.mk
+++ linux/linux.mk
@@ -461,7 +461,7 @@ endef
 endif # BR2_LINUX_KERNEL_APPENDED_DTB
 ifeq ($(BR2_LINUX_KERNEL_INSTALL_INTREE_OVERLAYS),y)
 define LINUX_INSTALL_OVERLAYS
-	install -t $(1)/overlays/ \
+	install -D -t $(1)/overlays/ \
 			$(wildcard $(LINUX_ARCH_PATH)/boot/dts/overlays/*.dtbo)
 endef
 endif # BR2_LINUX_KERNEL_INSTALL_INTREE_OVERLAYS
@@ -550,6 +550,7 @@ endef
 define LINUX_INSTALL_IMAGES_CMDS
 	$(call LINUX_INSTALL_IMAGE,$(BINARIES_DIR))
 	$(call LINUX_INSTALL_DTB,$(BINARIES_DIR))
+	$(call LINUX_INSTALL_OVERLAYS,$(BINARIES_DIR))
 endef
 
 ifeq ($(BR2_STRIP_strip),y)

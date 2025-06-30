eb49dd0b3e7cbf00ec3849349251b9798a7d7f43
Stefan Agner <stefan@agner.ch>
2025-06-30 20:55:03 +0200

    package/docker-engine: Fix go module version information
    When using Docker engine built by Buildroot, the BuildKit version
    information appears as "v0.0.0+unknown" instead of the actual version
    (e.g., "v0.22.0").
    
    This occurs because Buildroot's build process doesn't preserve Go module
    version information that Docker reads at runtime via debug.ReadBuildInfo()
    (see builder/builder-next/worker/worker.go).
    
    Docker's own build system (hack/make/.binary) uses "-mod=vendor
    -modfile=vendor.mod" flags, which are crucial for preserving module
    dependency information. Adding these flags makes the previous vendoring
    workaround obsolete for modern Docker versions.
    
    Note: Use "docker buildx ls" to verify the BuildKit version. This requires
    a Docker client with the buildx plugin installed.
    
    Signed-off-by: Stefan Agner <stefan@agner.ch>

--- package/docker-engine/docker-engine.mk
+++ package/docker-engine/docker-engine.mk
@@ -25,6 +25,7 @@ DOCKER_ENGINE_LDFLAGS = \
 
 DOCKER_ENGINE_TAGS = cgo exclude_graphdriver_zfs
 DOCKER_ENGINE_BUILD_TARGETS = cmd/dockerd cmd/docker-proxy
+DOCKER_ENGINE_BUILD_OPTS += -mod=vendor -modfile=vendor.mod
 
 ifeq ($(BR2_PACKAGE_LIBAPPARMOR),y)
 DOCKER_ENGINE_DEPENDENCIES += libapparmor
@@ -50,15 +51,6 @@ DOCKER_ENGINE_DEPENDENCIES += gvfs
 else
 DOCKER_ENGINE_TAGS += exclude_graphdriver_vfs
 endif
-
-# create the go.mod file with language version go1.19
-# remove the conflicting vendor/modules.txt
-# https://github.com/moby/moby/issues/44618#issuecomment-1343565705
-define DOCKER_ENGINE_FIX_VENDORING
-	printf "module $(DOCKER_ENGINE_GOMOD)\n\ngo 1.19\n" > $(@D)/go.mod
-	rm -f $(@D)/vendor/modules.txt
-endef
-DOCKER_ENGINE_POST_EXTRACT_HOOKS += DOCKER_ENGINE_FIX_VENDORING
 
 DOCKER_ENGINE_INSTALL_BINS = $(notdir $(DOCKER_ENGINE_BUILD_TARGETS))
 

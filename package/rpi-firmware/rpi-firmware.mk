c80dabe55e84cee3dc6de0a52d2f624dc3fb0752
Jan Čermák <sairon@sairon.cz>
2026-05-28 15:58:33 +0200

    package/rpi-firmware: update to 09267f5 (1.20260521)
    Update firmware to version matching the 6.18.32 kernel.
    
    Signed-off-by: Jan Čermák <sairon@sairon.cz>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Please keep in sync with configs/raspberrypi*_deconfig
-RPI_FIRMWARE_VERSION = eb3ee430dd1c96a9415c4be3657d8f0e976f8102
+RPI_FIRMWARE_VERSION = 09267f5354d40519d82fbd2193b9e211ec304055
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom

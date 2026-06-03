da2546993b3a155e94bb4aa8909b6f3c4f462e9c
Jan Čermák <sairon@sairon.cz>
2026-06-03 15:06:59 +0200

    package/rpi-firmware: update to 12eeaa1 (for stable_20260527)
    Update to version matching 6.18.33 stable release.
    
    Signed-off-by: Jan Čermák <sairon@sairon.cz>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Please keep in sync with configs/raspberrypi*_deconfig
-RPI_FIRMWARE_VERSION = 09267f5354d40519d82fbd2193b9e211ec304055
+RPI_FIRMWARE_VERSION = 12eeaa12865869b07db760f4bbb7507ec6f1976c
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom

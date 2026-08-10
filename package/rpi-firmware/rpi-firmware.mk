a2aa1edac11a8dd934ffbde37fb2e5557564a99a
Jan Čermák <sairon@sairon.cz>
2026-08-10 11:27:33 +0200

    package/rpi-firmware: update to 78e81e2 (for stable_20260724)
    Update to version matching 6.18.39 stable release.
    
    Signed-off-by: Jan Čermák <sairon@sairon.cz>

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Please keep in sync with configs/raspberrypi*_deconfig
-RPI_FIRMWARE_VERSION = 89752d47a688ec33c9f0c38f6549f627e5c143f9
+RPI_FIRMWARE_VERSION = 78e81e2cd6e00efeb79c169bff29ec457fa14b11
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3-Clause
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
